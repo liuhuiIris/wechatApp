155030a37e38609f8caa161b4d3e166403cdae53
liuhui <yuanwei5062@outlook.com>
2017-01-04 11:35:21 +0800

    fekit

---
+++ src/js/common.js
@@ -0,0 +1,24 @@
+var html = require('./tpls/index.string');
+var x = {
+	a:'aaa',
+	sayA:function() {
+		return this.a;
+	},
+	fetch:function(){
+		fetch('/api/list.php')
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(res) {
+			console.log(res);
+		})
+	},
+	render:function() {
+		console.log(html);
+		var body = document.body;
+		body.innerHTML = html+body.innerHTML;
+	}
+
+}
+
+module.exports = x;
